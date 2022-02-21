aa9354938dad75527f95f334353ef493ade97cea
Abdulrahman Darwish <abdulrahman.moustafa@gmail.com>
2022-02-21 09:44:25 +0200

    Add files via upload

---
+++ STE/loss.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  8 09:47:54 2022
+
+@author: The code is originally owned by https://github.com/SilvioGiancola/SoccerNetv2-DevKit/tree/main/Task1-ActionSpotting/TemporallyAwarePooling
+
+It was modified to fit our new model
+"""
+
+######################################################################################################
+
+#The code is originally owned by https://github.com/SilvioGiancola/SoccerNetv2-DevKit/tree/main/Task1-ActionSpotting/TemporallyAwarePooling
+#It was modified to fit our new model
+######################################################################################################""
+
+import torch
+
+
+class NLLLoss(torch.nn.Module):
+    def __init__(self):
+        super(NLLLoss, self).__init__()
+
+    def forward(self, labels, output):
+        # return torch.mean(labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output))
+      #  return torch.mean(torch.mean(labels * -torch.log(output) + (1 - labels) * -torch.log(1 - output)))
+        return torch.mean(torch.mean(-torch.special.xlogy(labels.float(),output.float()) - torch.special.xlogy(1.0-labels.float(),1.0-output.float())))
+        
+        
